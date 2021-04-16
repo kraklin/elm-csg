926c24169f2576cd83a003ad547b0f04bc4194ec
Tomas Latal <latal.tomas@gmail.com>
2021-04-16 22:38:40 +0200

    First tests for intersecting plane

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,8 +1,58 @@
 module Tests exposing (..)
 
+import Direction3d
+import Expect
+import Length exposing (Meters)
+import Plane3d exposing (Plane3d)
+import Point3d exposing (Point3d)
 import Test exposing (Test)
+import Triangle3d exposing (Triangle3d)
+import Utils exposing (IntersectionResult(..))
 
 
 suite : Test
 suite =
-    Test.todo "Implement the first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    Test.describe "Plane-triangle intersections"
+        [ Test.test "Coplanar triangle should have all points intersected" <|
+            \_ ->
+                let
+                    triangle =
+                        Triangle3d.from
+                            (Point3d.meters 0 0 0)
+                            (Point3d.meters 1 0 0)
+                            (Point3d.meters 1 1 0)
+
+                    plane =
+                        Plane3d.through (Point3d.meters 0 0 0) Direction3d.z
+                in
+                Utils.planeTriangleIntersection plane triangle
+                    |> Expect.equal Coplanar
+        , Test.test "Triangle outside of the plane has no points intersected" <|
+            \_ ->
+                let
+                    triangle =
+                        Triangle3d.from
+                            (Point3d.meters 0 0 0)
+                            (Point3d.meters 1 0 0)
+                            (Point3d.meters 1 1 0)
+
+                    plane =
+                        Plane3d.through (Point3d.meters 0 0 1) Direction3d.negativeZ
+                in
+                Utils.planeTriangleIntersection plane triangle
+                    |> Expect.equal Utils.None
+        , Test.test "Triangle on the other side of the plane has no points intersected" <|
+            \_ ->
+                let
+                    triangle =
+                        Triangle3d.from
+                            (Point3d.meters 0 0 0)
+                            (Point3d.meters 1 0 0)
+                            (Point3d.meters 1 1 0)
+
+                    plane =
+                        Plane3d.through (Point3d.meters 0 0 -1) Direction3d.negativeZ
+                in
+                Utils.planeTriangleIntersection plane triangle
+                    |> Expect.equal Utils.None
+        ]

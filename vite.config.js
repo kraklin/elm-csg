b11a912d4e57e93a2b38d68d85b8505e94826720
Tomáš  Látal <latal.tomas@gmail.com>
2022-08-01 09:15:55 +0200

    Replace functions names for reading performance better

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { plugin } from 'vite-plugin-elm'
+import Elm from 'vite-plugin-elm'
+import StringReplace from 'vite-plugin-string-replace'
 
 export default defineConfig({
-    plugins: [plugin({debug: false})]
+    plugins: [
+        Elm({debug: false}),
+        StringReplace([
+            { search: /(\sA\d\([\s]+)([^,]+)(,[\s]+)(function[^(]+)\(/gm, replace:`$1$2$3$4___$2(` },
+            { search: /var ([^=]+)( = F\d\([^f]+function)[^(]\(/gmi, replace: `var $1$2 __$1( `}
+        ])
+
+    ]
 })
 
